318b4914986b65af005e1e7f7a624e32227bffb1
R41NB0W-D4SH <54800479+R41NB0W-D4SH@users.noreply.github.com>
2019-11-14 22:29:47 +0300

    Update task1.cpp

--- task1.cpp
+++ task1.cpp
@@ -10,15 +10,22 @@ int main()
 	cout << "Введите число N" << endl;
 	cin >> n;
 
-	int *arr = new int[n];
-	for (i = 0, j = 1; i < n; i++, j += 2)
-	{
-		arr[i] = j;
-	}
+	if (n > 0)
+	{ 
+		int *arr = new int[n];
+		for (i = 0, j = 1; i < n; i++, j += 2)
+		{
+			arr[i] = j;
+		}
 
-	for (i = 0; i < n; i++)
+		for (i = 0; i < n; i++)
+		{
+			cout << "Элемент " << i << ":" << arr[i] << endl;
+		}
+	}
+	else
 	{
-		cout << "Элемент " << i << ":" << arr[i] << endl;
+		cout << "N по условию больше нуля. Попробуйте еще раз ввести N, следуя условию." << endl;
 	}
 
 	system("pause");
