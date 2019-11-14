f786df50f2f27da3328d042ed1c770e364a8718e
R41NB0W-D4SH <54800479+R41NB0W-D4SH@users.noreply.github.com>
2019-11-14 22:27:43 +0300

    Create task1.cpp

---
+++ task1.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(0, "");
+	int n, i, j;
+
+	cout << "Введите число N" << endl;
+	cin >> n;
+
+	int *arr = new int[n];
+	for (i = 0, j = 1; i < n; i++, j += 2)
+	{
+		arr[i] = j;
+	}
+
+	for (i = 0; i < n; i++)
+	{
+		cout << "Элемент " << i << ":" << arr[i] << endl;
+	}
+
+	system("pause");
+	return 0;
+}
