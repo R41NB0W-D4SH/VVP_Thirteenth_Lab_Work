3f2d135e7ef5e9e91a8a54a85e6410b210e098f4
R41NB0W-D4SH <54800479+R41NB0W-D4SH@users.noreply.github.com>
2019-11-15 00:33:33 +0300

    Update task5.cpp

--- task5.cpp
+++ task5.cpp
@@ -1,1 +1,33 @@
+#include <iostream>
+#include <cmath>
 
+using namespace std;
+
+int main()
+{
+	setlocale(0, "");
+	int n, i;
+
+	cout << "Введите размерность массива: ";
+	cin >> n;
+	
+	int *a = new int[n];
+	
+	for (i = 0; i < n; i++)
+	{
+		cout << "Введите элемент массива: ";
+		cin >> a[i];
+	}
+	for (i = 0; i < n; i += 2) 
+	{
+		cout << a[i] << ", ";
+	}
+
+	for (i = n - 1; i > 0; i -= 2)
+	{
+		cout << a[i] << ", ";
+	}
+
+	system("pause");
+	return 0;
+}
