e3f93b1279c2d31c74ba4385828a458b11d1a9ff
R41NB0W-D4SH <54800479+R41NB0W-D4SH@users.noreply.github.com>
2019-12-03 12:47:03 +0300

    Update task3.cpp

--- task3.cpp
+++ task3.cpp
@@ -1,35 +1,37 @@
 #include <iostream>
-#include <cmath>
 
 using namespace std;
 
 int main()
 {
 	setlocale(0, "");
-	int a, b, n, i;
+	int a, b, n, i, dp;
 
-	cout << "Введите первый элемент массива" << endl;
+	cout << "Введите первый элемент массива: ";
 	cin >> a;
-	cout << "Введите второй элемент массива" << endl;
+	cout << "Введите второй элемент массива: ";
 	cin >> b;
-	cout << "Введите размерность массива" << endl;
+	cout << "Введите размерность массива: ";
 	cin >> n;
-	
+
 
 	if (n > 2)
-	{ 
+	{
 		int *arr = new int[n];
 		arr[0] = a;
 		arr[1] = b;
 
+		dp = arr[0] + arr[1];
+
 		for (i = 2; i < n; i++)
 		{
-			arr[i] = arr[i - 1] + arr[i - 2];
+			arr[i] = dp;
+			dp = dp + arr[i];
 		}
 
 		for (i = 0; i < n; i++)
 		{
-			cout << "Элемент " << i << ":" << arr[i] << endl;
+			cout << "a[" << i << "] : " << arr[i] << endl;
 		}
 	}
 	else
